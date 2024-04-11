11c3cc5e306469cfe245c5409908e56895560186
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-11 13:00:04 +0200

    Add files via upload

---
+++ S05T01N01MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n01/model/domain/Branch.java
@@ -0,0 +1,50 @@
+package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n01.model.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "branch")
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pk_BranchID;
+
+    @Column(name = "name")
+    private String nameBranch;
+
+    @Column(name = "country")
+    private String countryBranch;
+
+    public Branch() {}
+
+
+    public Branch (String nameBranch, String countryBranch){
+        this.nameBranch = nameBranch;
+        this.countryBranch = countryBranch;
+    }
+
+    public Integer getPk_BranchID() {
+        return pk_BranchID;
+    }
+
+    public String getNameBranch() {
+        return nameBranch;
+    }
+
+    public String getCountryBranch() {
+        return countryBranch;
+    }
+
+    public void setPk_BranchID(Integer pk_BranchID) {
+        this.pk_BranchID = pk_BranchID;
+    }
+
+    public void setNameBranch(String nameBranch) {
+        this.nameBranch = nameBranch;
+    }
+
+    public void setCountryBranch(String countryBranch) {
+        this.countryBranch = countryBranch;
+    }
+}
