78b168b43378fba9b67dd2f1e429bb1b77db47b5
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-15 13:22:52 +0200

    Delete S05T01N01MonzoJorge directory

--- S05T01N01MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n01/model/repository/BranchRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n01.model.repository;
-
-import cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n01.model.domain.Branch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BranchRepository extends JpaRepository <Branch, Integer> {
-}
