388a2e7666538950e7cc53b0e8f4314f2ffdca72
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-18 12:32:39 +0200

    Delete S05T01N02MonzoJorge directory

--- S05T01N02MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n02/exceptions/FlowerIsNull.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n02.exceptions;
-
-public class FlowerIsNull extends RuntimeException{
-
-    public FlowerIsNull() {
-        super("Error data cannot by empty");
-
-    }
-}
--- S05T01N02MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n02/exceptions/FlowerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n02.exceptions;
-
-public class FlowerNotFoundException extends RuntimeException {
-
-    public FlowerNotFoundException(Integer id) {
-        super("Flower not found with ID: "+id);
-    }
-}
