81581c52585c61d901f61903a173166242dfc830
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-20 10:41:52 +0200

    Add files via upload

---
+++ S05T01N03MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n03/exceptions/FlowerIsNull.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n03.exceptions;
+
+    public class FlowerIsNull extends RuntimeException{
+
+        public FlowerIsNull() {
+            super("Error data cannot by empty");
+
+        }
+    }
+
