aa63183abdf85931a826693eacc8324aae16e152
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-04-22 11:03:14 +0200

    Delete S05T01N03MonzoJorge directory

--- S05T01N03MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n03/exceptions/FlowerNotCreated.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n03.exceptions;
-
-public class FlowerNotCreated extends RuntimeException{
-    public FlowerNotCreated (){
-        super("Can't create a new flower at BBDD, check the information you send");
-    }
-}
--- S05T01N03MonzoJorge/src/main/java/cat/itacademy/barcelonactiva/monzo/jorge/s05/t01/n03/exceptions/FlowerNotGetAll.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.monzo.jorge.s05.t01.n03.exceptions;
-
-public class FlowerNotGetAll extends RuntimeException{
-
-    public FlowerNotGetAll(){
-        super("The flowersdb is empty");
-    }
-
-}
